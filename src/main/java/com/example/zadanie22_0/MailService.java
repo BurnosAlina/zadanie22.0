9eded77f0c19f829ae22e9f184b8613d82d325a1
Alina Burnos <burnosalina@gmail.com>
2023-04-24 07:39:46 +0200

    zadanie22.0 poprawka 2

--- src/main/java/com/example/zadanie22_0/MailService.java
+++ src/main/java/com/example/zadanie22_0/MailService.java
@@ -14,17 +14,27 @@ public class MailService {
     }
 
     public void send(String name, String email, String mailText) {
+        sendMailToAdmin(name, mailText, email);
+        sendConfirmationMail(email);
+
+    }
+
+    private void sendConfirmationMail(String email) {
+        SimpleMailMessage confirmationMail = new SimpleMailMessage();
+        confirmationMail.setTo(email);
+        confirmationMail.setFrom("noreply@op.pl");
+        confirmationMail.setSubject("Potwierdzenie wysłania zapytania");
+        confirmationMail.setText("Dziękujemy za wysłanie zapytania. Odpowiemy wkrótce.");
+        javaMailSender.send(confirmationMail);
+    }
+
+    private void sendMailToAdmin(String name, String mailText, String email) {
         SimpleMailMessage mail = new SimpleMailMessage();
         mail.setSubject("Wiadomość od użytkownika " + name);
         mail.setTo("example1@op.pl");
         mail.setText(mailText);
-        mail.setFrom(name);
+        mail.setFrom(email);
         mail.setReplyTo(email);
         javaMailSender.send(mail);
-        SimpleMailMessage confirmationMail = new SimpleMailMessage();
-        confirmationMail.setTo(email);
-        confirmationMail.setSubject("Potwierdzenie wysłania zapytania");
-        confirmationMail.setText("Dziękujemy za wysłanie zapytania. Odpowiemy wkrótce.");
-        javaMailSender.send(confirmationMail);
     }
 }
